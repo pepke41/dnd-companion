58392a75b348a6ee41d990a6e099b348005ead85
Emily Pepke <elpepke41@gmail.com>
2017-11-07 23:08:09 -0800

    firebase permission issue

--- app/routes/player-handbook.js
+++ app/routes/player-handbook.js
@@ -1,7 +1,11 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  session: service(),
   model: function() {
-    return this.store.findAll('world-info');
-  },
+    if (this.get('session.isAuthenticated')) {
+      return this.store.findAll('world-info');
+    }
+  }
 });
