349d19c1469b3ab466c8982ac0466f15ee0e5927
Emily Pepke <elpepke41@gmail.com>
2017-11-19 17:52:17 -0800

    ui updates to player-handbook page

--- app/controllers/player-handbook.js
+++ app/controllers/player-handbook.js
@@ -6,15 +6,31 @@ export default Controller.extend({
   friendly: computed.filterBy('model', 'type', 'friendly'),
   enemy: computed.filterBy('model', 'type', 'enemy'),
   other: computed.filterBy('model', 'type', 'other'),
+  addInfoDialog: false,
+  infoType: '',
 
   actions: {
-    appendToList(type, name, description) {
+    appendToList() {
       var newInfo = this.store.createRecord('world-info', {
-        name: name,
-        description: description,
-        type: type
+        name: this.get("nameValue"),
+        description: this.get("descriptionValue"),
+        type: this.get("infoType")
       });
-      return newInfo.save();
+      return newInfo.save().then(() => {
+        this.set("addInfoDialog", false);
+        this.set("nameValue", undefined);
+        this.set("descriptionValue", undefined);
+      });
+    },
+    openDialog(name) {
+      this.set("addInfoDialog", true);
+      this.set("infoType", name);
+    },
+    closeDialog() {
+      this.set("addInfoDialog", false);
+    },
+    deleteItem(item) {
+      item.destroyRecord();
     }
   }
 });
