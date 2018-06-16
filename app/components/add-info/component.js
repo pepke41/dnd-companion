a2e7fb66cb3a8e0151b0f62b28dd3d605de159d8
Emily Pepke <elpepke41@gmail.com>
2018-06-16 15:15:18 -0700

    added text enhancements

--- app/components/add-info/component.js
+++ app/components/add-info/component.js
@@ -1,7 +1,22 @@
 import Component from '@ember/component';
+import Ember from 'ember';
 
 export default Component.extend({
   type: undefined,
   title: undefined,
-  description: undefined
+  description: undefined,
+  events: null,
+
+  initializeEvents: Ember.on('init', function () {
+    this.set('events', Ember.A());
+  }),
+
+  actions: {
+    handleTrixAction(jqEvent) {
+      this.get('events').unshiftObject(Ember.Object.create({
+        type: jqEvent.type
+      }));
+      this.set('descriptionValue', jqEvent.target.value);
+    }
+  }
 });
